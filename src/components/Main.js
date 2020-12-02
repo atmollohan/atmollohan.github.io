9ba08473e277f1abb433f84bf1f93bdd629c1835
atmollohan <atmollohan@gmail.com>
2020-12-01 20:30:56 -0500

    paragraph change

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,11 +33,7 @@ class Main extends React.Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+            From the North East
           </p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
