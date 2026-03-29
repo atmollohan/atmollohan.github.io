d95b28fb39cbe1f015f998501defaeee9bb3ccbc
Andrew Mollohan <atmollohan@gmail.com>
2026-03-29 15:29:30 -0400

    remove confetti, center twins image, remove work section image

--- src/components/Confetti.js
+++ src/components/Confetti.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
 
-const ConfettiDrop = () => {
-  const { width, height } = useWindowSize()
+const ConfettiDrop = ({ height = 400 }) => {
+  const { width } = useWindowSize()
+
   return (
-    <Confetti
-      style={{ width: '100%', height: '100%' }}
-      width={width}
-      height={height}
-      initialVelocityY={5}
-    />
+    <div
+      style={{
+        position: 'relative',
+        height: `${height}px`,
+        overflow: 'hidden',
+      }}
+    >
+      <Confetti
+        style={{ position: 'absolute', top: 0, left: 0 }}
+        width={Math.min(width, 400)}
+        height={height}
+        numberOfPieces={50}
+        initialVelocityY={5}
+        recycle={false}
+        gravity={0.15}
+      />
+    </div>
   )
+}
+
+ConfettiDrop.propTypes = {
+  height: PropTypes.number,
 }
 
 export default ConfettiDrop
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import Confetti from './Confetti'
+import { StaticQuery, graphql } from 'gatsby'
 import SocialLinks from './SocialLinks'
 import twins from '../images/fampiece-crop.jpg'
-import resume from '../content/resume.current.v1.7.pdf'
 
-const Intro = ({ article, articleTimeout, onCloseArticle }) => {
+const IntroContent = ({ article, articleTimeout, onCloseArticle, html }) => {
   const [response, setResponse] = useState('')
 
   const close = (
@@ -34,39 +33,7 @@ const Intro = ({ article, articleTimeout, onCloseArticle }) => {
       style={{ display: 'none' }}
     >
       <h2 className="major">Intro</h2>
-      <p>
-        Oh... I didn&apos;t see you there. It looks like you stumbled across my
-        portfolio. Feel free to look around or, if you&apos;re in a rush, grab a
-        copy of my{' '}
-        <a href={resume} target="_blank" rel="noreferrer" title="atmollohan">
-          resume
-        </a>
-        . A resume only gives you a page&apos;s worth of information. So I
-        figured it would be better to wear my JS hat and build a portfolio with
-        modern frameworks. This is a good space for me to show what I can do in
-        both my 9-5 and my 5-9.
-      </p>
-      <p>
-        Here are some technical areas of interest:
-        <ul>
-          <li>Cloud Computing</li>
-          <li>Self Hosting</li>
-          <li>Application Development</li>
-          <li>AI / ML Development</li>
-          <li>Computer Hardware</li>
-          <li>Workflow Automation</li>
-          <li>Software Security</li>
-          <li>Data Engineering</li>
-          <li>Decentralized Finance</li>
-          <li>Ecommerce</li>
-          <li>Digital Design</li>
-        </ul>
-      </p>
-      <p>
-        Reach out if you want to collaborate or just take a look around and let
-        me know what you think!
-      </p>
-      {response === 'left' && <Confetti />}
+      <div dangerouslySetInnerHTML={{ __html: html }} />
       <p>Can you guess which one is me?</p>
       <div>
         <div>
@@ -80,38 +47,57 @@ const Intro = ({ article, articleTimeout, onCloseArticle }) => {
         <br></br>
         {response === 'right' && <p>It&apos;s okay... I forgive you.</p>}
         {response === 'left' && <p>I knew you could do it!</p>}
-        <span className="image main">
-          <img src={twins} alt="twins" />
-        </span>
+        <div
+          className="image main"
+          style={{
+            position: 'relative',
+            display: 'block',
+            textAlign: 'center',
+            margin: '0 auto',
+          }}
+        >
+          <img
+            src={twins}
+            alt="twins"
+            style={{
+              display: 'block',
+              maxWidth: '100%',
+              margin: '0 auto',
+              animation:
+                response === 'left' ? 'bounce 0.6s ease-in-out' : 'none',
+            }}
+          />
+        </div>
         <br></br>
         <p>I grew up as one of a pair. Believe it or not, we are fraternal.</p>
       </div>
-      <p>
-        In case you&apos;re curious about this site specifically, I used a
-        React-based, open-source framework called Gatsby using a fairly minimal{' '}
-        <a
-          href="https://github.com/codebushi/gatsby-starter-dimension"
-          target="_blank"
-          rel="noreferrer"
-        >
-          design
-        </a>{' '}
-        that I pulled from their templates. I have the source code on{' '}
-        <a
-          href="https://github.com/atmollohan/atmollohan.github.io"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{' '}
-        and am deploying it automatically on commits to main with GitHub
-        Actions.
-      </p>
       <SocialLinks />
       {close}
     </article>
   )
 }
+
+IntroContent.propTypes = {
+  article: PropTypes.string,
+  articleTimeout: PropTypes.bool,
+  onCloseArticle: PropTypes.func,
+  html: PropTypes.string,
+}
+
+const Intro = (props) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        markdownRemark(frontmatter: { slug: { eq: "/intro" } }) {
+          html
+        }
+      }
+    `}
+    render={(data) => (
+      <IntroContent html={data.markdownRemark.html} {...props} />
+    )}
+  />
+)
 
 Intro.propTypes = {
   article: PropTypes.string,
