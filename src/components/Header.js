659c8a898bb36cdb4c4dfce4166a5439aa70a524
Andrew Mollohan <atmollohan@gmail.com>
2026-04-04 19:24:48 -0400

    clean up header styling, speed up background fade, remove logo icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = (props) => (
   <header id="header" style={!props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-asterisk"></span>
+      {/* TODO: Add Mollo Tech logo - generate SVG logo with amber accent */}
     </div>
     <div className="content">
       <div className="inner">
